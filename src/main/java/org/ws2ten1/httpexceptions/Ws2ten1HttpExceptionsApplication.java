0d764597de8cd34889cf7e3f7b1e08288c6a8f39
Daisuke Miyamoto <dai.0304@gmail.com>
2019-02-23 12:15:43 +0900

    initial source from spring initializr

---
+++ src/main/java/org/ws2ten1/httpexceptions/Ws2ten1HttpExceptionsApplication.java
@@ -0,0 +1,13 @@
+package org.ws2ten1.httpexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws2ten1HttpExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws2ten1HttpExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ws2ten1/httpexceptions/Ws2ten1HttpExceptionsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ws2ten1.httpexceptions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ws2ten1HttpExceptionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
